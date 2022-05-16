95c6eca235cec37317c56e0bfbef6376e65e8215
rkolx <olx.rko.o@gmail.com>
2022-05-17 08:39:36 +0900

    feat: 프로젝트 환경설정
    프로젝트 생성: view 환경 설정, jpa와 db 설정 및 동작 확인

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        //findMember == member -> ture (같은 트랜잭션 내에서 저장하고 조회하면 같은 영속성 컨텍스트에서는 id(식별자)가 같으면 같은 엔티티로 식별)
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
